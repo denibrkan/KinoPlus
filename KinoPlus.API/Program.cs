12910bb6dda49d7d534083b5fbd95a8694c1a05e
Deni <brkandeni@gmail.com>
2023-05-15 23:31:09 +0200

    docker compose

--- KinoPlus.API/Program.cs
+++ KinoPlus.API/Program.cs
@@ -85,11 +85,9 @@ builder.Services.AddScoped<IRecommendationService, RecommendationService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
