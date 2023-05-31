741450b1b55cd8d832c25de509144d8f8e3e1e24
Deni <brkandeni@gmail.com>
2023-05-31 11:19:34 +0200

    cleanup program.cs

---
+++ KinoPlus.API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,104 @@
+﻿using KinoPlus.Services;
+using KinoPlus.Services.Database;
+using KinoPlus.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+namespace KinoPlus.API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static void AddApplicationServices(
+            this IServiceCollection services,
+            IConfiguration config
+        )
+        {
+            services.AddDbContext<KinoplusContext>(
+                options => options.UseSqlServer(config.GetConnectionString("DefaultConnection"))
+            );
+
+            services.AddAutoMapper(typeof(Program));
+
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IImageService, ImageService>();
+
+            services.AddScoped<IMovieService, MovieService>();
+            services.AddScoped<IMovieStatusService, MovieStatusService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IActorService, ActorService>();
+            services.AddScoped<IGenreService, GenreService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IYearService, YearService>();
+            services.AddScoped<IProjectionService, ProjectionService>();
+            services.AddScoped<IProjectionTypeService, ProjectionTypeService>();
+            services.AddScoped<ILocationService, LocationService>();
+            services.AddScoped<ICityService, CityService>();
+            services.AddScoped<IHallService, HallService>();
+            services.AddScoped<ISeatService, SeatService>();
+            services.AddScoped<ITicketService, TicketService>();
+            services.AddScoped<IWeekDayService, WeekDayService>();
+            services.AddScoped<IMovieReactionService, MovieReactionService>();
+            services.AddScoped<IRecommendationService, RecommendationService>();
+        }
+
+        public static void AddBearerAuthentication(
+            this IServiceCollection services,
+            IConfiguration config
+        )
+        {
+            services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(config["TokenKey"]!)
+                        ),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+        }
+
+        public static void AddSwaggerWithAuthorization(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition(
+                    "Bearer",
+                    new OpenApiSecurityScheme()
+                    {
+                        Name = "Authorization",
+                        Type = SecuritySchemeType.ApiKey,
+                        Scheme = "Bearer",
+                        BearerFormat = "JWT",
+                        In = ParameterLocation.Header,
+                        Description =
+                            "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+                    }
+                );
+                c.AddSecurityRequirement(
+                    new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            new string[] { }
+                        }
+                    }
+                );
+            });
+        }
+    }
+}
--- KinoPlus.API/Program.cs
+++ KinoPlus.API/Program.cs
@@ -1,12 +1,8 @@
-using KinoPlus.Services;
+using KinoPlus.API.Extensions;
 using KinoPlus.Services.Data;
 using KinoPlus.Services.Database;
 using KinoPlus.Services.Interfaces;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using System.Text;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,70 +12,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews()
                 .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
-        {
-            new OpenApiSecurityScheme {
-                Reference = new OpenApiReference {
-                    Type = ReferenceType.SecurityScheme,
-                        Id = "Bearer"
-                }
-            },
-            new string[] {}
-        }
-    });
-});
 
+builder.Services.AddBearerAuthentication(builder.Configuration);
+builder.Services.AddSwaggerWithAuthorization();
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-              .AddJwtBearer(options =>
-              {
-                  options.TokenValidationParameters = new TokenValidationParameters
-                  {
-                      ValidateIssuerSigningKey = true,
-                      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"]!)),
-                      ValidateIssuer = false,
-                      ValidateAudience = false
-                  };
-              });
-
-builder.Services.AddDbContext<KinoplusContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddAutoMapper(typeof(Program));
-
-builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddScoped<IImageService, ImageService>();
-
-builder.Services.AddScoped<IMovieService, MovieService>();
-builder.Services.AddScoped<IMovieStatusService, MovieStatusService>();
-builder.Services.AddScoped<ICategoryService, CategoryService>();
-builder.Services.AddScoped<IActorService, ActorService>();
-builder.Services.AddScoped<IGenreService, GenreService>();
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IYearService, YearService>();
-builder.Services.AddScoped<IProjectionService, ProjectionService>();
-builder.Services.AddScoped<IProjectionTypeService, ProjectionTypeService>();
-builder.Services.AddScoped<ILocationService, LocationService>();
-builder.Services.AddScoped<ICityService, CityService>();
-builder.Services.AddScoped<IHallService, HallService>();
-builder.Services.AddScoped<ISeatService, SeatService>();
-builder.Services.AddScoped<ITicketService, TicketService>();
-builder.Services.AddScoped<IWeekDayService, WeekDayService>();
-builder.Services.AddScoped<IMovieReactionService, MovieReactionService>();
-builder.Services.AddScoped<IRecommendationService, RecommendationService>();
+builder.Services.AddApplicationServices(builder.Configuration);
 
 
 var app = builder.Build();
